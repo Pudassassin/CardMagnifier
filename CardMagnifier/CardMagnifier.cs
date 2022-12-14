2e17b2b44ddaac5f943ce04034c257f0eb4dc2cf
Wachirawit Soontornworachan <pud_wachirawit@hotmail.com>
2022-12-14 11:49:56 +0700

    Functional prototype

---
+++ CardMagnifier/CardMagnifier.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System;
+
+using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+
+using CardMagnifier.MonoBehaviors;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+using UnityEngine;
+
+namespace CardMagnifier
+{
+    // These are the mods required for our mod to work
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class CardMagnifier : BaseUnityPlugin
+    {
+        private const string ModId = "com.pudassassin.rounds.CardMagnifier";
+        private const string ModName = "CardMagnifier";
+        public const string Version = "0.0.3"; //build #3 / Release 0-1-0
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            // CustomCard.BuildCard<MyCardName>();
+            Unbound.RegisterClientSideMod(ModId);
+
+            this.ExecuteAfterSeconds(2.0f, () =>
+            {
+                // Debug.Log("[CardMagnifier] Delegate started!");
+
+                GameObject targetCardBase = null;
+                GameObject[] gameObjects = Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[];
+
+
+                if (gameObjects != null)
+                {
+                    foreach (GameObject item in gameObjects)
+                    {
+                        // if (item == null) continue;
+                        // if (item.hideFlags == HideFlags.HideAndDontSave)
+                        // {
+                            if (item.name == "CardBase")
+                            {
+                                // Debug.Log("[CardMagnifier] Found it!");
+                                targetCardBase = item;
+                                break;
+                            }
+                        // }
+                    }
+                }
+                else
+                {
+                    // Debug.Log("[CardMagnifier] List is null!");
+                }
+
+                // apply mono to card base(s)
+                if (targetCardBase != null)
+                {
+                    targetCardBase.AddComponent<CardEnlarger>();
+                    // Debug.Log("[CardMagnifier] Component added!");
+                }
+            });
+        }
+    }
+
+
+
+}
---
+++ CardMagnifier/MonoBehaviors/CardEnlarger.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+using UnboundLib.GameModes;
+
+namespace CardMagnifier.MonoBehaviors
+{
+    // this mono is meant to be attached at vanilla CardBase and potentially customized ones from mods
+    // Card is clone when pick card phase start and offering player, and is destroyed afterward
+
+    public class CardEnlarger : MonoBehaviour
+    {
+        public static List<String> BlacklistedCards = new List<String>()
+        {
+            // PCE Glitch cards
+            "__PCE__Common",
+            "__PCE__Uncommon",
+            "__PCE__Rare"
+        };
+
+        public static Vector2Int defaultResolution = new Vector2Int(1920, 1080); // in case of adaptive zoom feature
+
+        // scale 0-1, 0 = original card pos, 1 = zoom to configured pos
+        public static float configPosInterpolateFactor = 0.95f;
+        public static Vector3 configZoomToPos = new Vector3(0.0f, 3.0f, -10.0f);
+
+        // whether to zoom the entire card to fixed scales or not, instead of relative to its default size when hilighted
+        public static bool configZoomAbsoluteEnable = true;
+        public static float configZoomScale = 1.35f;
+
+        public static bool configReorientCardEnable = true;
+        public static Vector3 configZoomRotation = Vector3.zero; // WIP
+
+        public static float configInterpolateTime = 0.2f;
+
+        public Vector3 cardZoomToPos, cardPreviousPos, cardPreviousRotation;
+        public Vector3 cardZoomToScale, cardPreviousScale;
+
+        public const float processTickTime = 0.0f;
+        public float processTimer = 0.0f;
+        public float interpolateTimer = 0.0f;
+        public bool effectEnabled = false; // should only be true during card picking phase
+        public bool cardIsHighLighted = false;
+        public bool cardPrevStateSaved = false;
+        public bool cardIsPicked = false;
+
+        private CardVisuals cardVisuals = null;
+        private GameObject cardBaseParent = null;
+
+        public void Awake()
+        {
+
+        }
+
+        public void Start()
+        {
+            cardVisuals = GetComponent<CardVisuals>();
+            cardBaseParent = transform.parent.gameObject;
+
+            Action<bool> action = GetToggleSelectionAction();
+            cardVisuals.toggleSelectionAction = (Action<bool>)Delegate.Combine(cardVisuals.toggleSelectionAction, action);
+        }
+
+        public void Update()
+        {
+            if (cardBaseParent != null && CardChoice.instance.IsPicking)
+            {
+                if (CheckCardIsBlacklisted())
+                {
+                    Destroy(this);
+                }
+                else
+                {
+                    processTimer += TimeHandler.deltaTime;
+                    // if (processTimer >= 3.0f)
+                    // {
+                    effectEnabled = true;
+                    
+                    if (!cardPrevStateSaved)
+                    {
+                        SetupCardEnlarger();
+                    }
+                    // }
+                }
+            }
+
+            if (effectEnabled)
+            {
+                if (cardIsHighLighted)
+                {
+                    interpolateTimer += TimeHandler.deltaTime;
+                }
+                else
+                {
+                    interpolateTimer -= TimeHandler.deltaTime;
+                }
+
+                interpolateTimer = Mathf.Clamp(interpolateTimer, 0.0f, configInterpolateTime);
+
+                if (!cardIsPicked)
+                {
+                    SetCardZoom(interpolateTimer);
+                }
+            }
+        }
+
+        public void SetupCardEnlarger()
+        {
+            cardPreviousPos = transform.parent.transform.position; //world space position
+            cardPreviousRotation = transform.parent.transform.localEulerAngles;
+            cardPreviousScale = transform.parent.transform.localScale;
+
+            cardZoomToPos = Vector3.Lerp(cardPreviousPos, configZoomToPos, configPosInterpolateFactor);
+            if (configZoomAbsoluteEnable)
+            {
+                cardZoomToScale = Vector3.one * configZoomScale;
+            }
+            else
+            {
+                cardZoomToScale = cardPreviousScale * configZoomScale;
+            }
+
+            cardPrevStateSaved = true;
+        }
+
+        private void SetCardZoom(float time)
+        {
+            float iValue = time / configInterpolateTime;
+            iValue = BinomialToLerpMapping(Mathf.Clamp01(iValue));
+
+            cardBaseParent.transform.localPosition = Vector3.Lerp(cardPreviousPos, cardZoomToPos, iValue);
+            cardBaseParent.transform.localScale = Vector3.Lerp(cardPreviousScale, cardZoomToScale, iValue);
+            if (configReorientCardEnable)
+            {
+                cardBaseParent.transform.localEulerAngles = V3RotationalLerp(cardPreviousRotation, configZoomRotation, iValue);
+            }
+        }
+
+        private Vector3 V3RotationalLerp(Vector3 start, Vector3 end, float t)
+        {
+            float tC = Mathf.Clamp01(t);
+            return new Vector3
+            (
+                Mathf.LerpAngle(start.x, end.x, tC),
+                Mathf.LerpAngle(start.y, end.y, tC),
+                Mathf.LerpAngle(start.z, end.z, tC)
+            );
+        }
+
+        private float BinomialToLerpMapping(float x)
+        {
+            // formula
+            float xC = Mathf.Clamp01(x);
+            return (-1.0f * xC * xC) + (2 * xC);
+        }
+
+        public IEnumerator OnPlayerPickStart(IGameModeHandler gm)
+        {
+            if (CheckCardIsBlacklisted())
+            {
+                effectEnabled = false;
+            }
+            else
+            {
+                effectEnabled = true;
+            }
+            yield break;
+        }
+
+        public IEnumerator OnPlayerPickEnd(IGameModeHandler gm)
+        {
+            cardIsPicked = true;
+            yield break;
+        }
+
+        public bool CheckCardIsBlacklisted()
+        {
+            foreach (String item in BlacklistedCards)
+            {
+                if (transform.parent.name.Contains(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Action<bool> GetToggleSelectionAction()
+        {
+            return delegate (bool highLightState)
+            {
+                if (effectEnabled)
+                {
+                    if (cardBaseParent == null)
+                    {
+                        cardBaseParent = transform.parent.gameObject;
+                        return;
+                    }
+
+                    if (!cardPrevStateSaved)
+                    {
+                        SetupCardEnlarger();
+                    }
+
+                    cardIsHighLighted = highLightState;
+                }
+            };
+        }
+
+        // debug
+        public void ResetTimer()
+        {
+            interpolateTimer = 0.0f;
+        }
+    }
+}
